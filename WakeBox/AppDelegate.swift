c4e9c2999bb9afaee2c232bb2801a58ef9f472b2
Jack Fischer <jfische8@binghamton.edu>
2015-11-14 18:10:59 -0500

    Initial Commit

---
+++ WakeBox/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  WakeBox
+//
+//  Created by Jack Fischer on 11/14/15.
+//  Copyright © 2015 trump6. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
